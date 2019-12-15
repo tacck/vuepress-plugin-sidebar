0b6ae47e3ef949034e4a43e18c832de0613c0c72
Takuya KIHARA <gray@tacck.net>
2019-12-15 15:55:18 +0900

    :sparkles: add option

--- enhanceAppFile.js
+++ enhanceAppFile.js
@@ -1,5 +1,12 @@
 export default ({ Vue, options, router, siteData }) => {
   const sidebar = []
+  let postItems = []
+
+  if (OPTION !== null && typeof OPTION === 'object') {
+    if ('postItems' in OPTION && Array.isArray(OPTION.postItems)) {
+      Array.prototype.push.apply(postItems, OPTION.postItems)
+    }
+  }
 
   // regularPath を使うと、ページトップの `#` で宣言した内容をタイトルとして使ってくれる。
   for (const page of siteData.pages) {
@@ -10,6 +17,9 @@ export default ({ Vue, options, router, siteData }) => {
   sidebar.sort((page1, page2) => {
     return page1.localeCompare(page2)
   })
+
+  // postItems
+  Array.prototype.push.apply(sidebar, postItems)
 
   siteData.themeConfig.sidebar = sidebar
 }
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
 module.exports = (option, context) => ({
-  enhanceAppFiles: path.resolve(__dirname, 'enhanceAppFile.js')
+  enhanceAppFiles: path.resolve(__dirname, 'enhanceAppFile.js'),
+  define: {
+    OPTION: option,
+    CONTEXT: context
+  }
 })
